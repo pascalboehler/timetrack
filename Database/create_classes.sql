7729b806176ebc8ba735299db53ca3fc947e4759
Pascal Böhler <pascal.boehler@hotmail.de>
2021-12-09 16:58:56 +0100

    feat: Added foreign key attribute and fixed time storage issue

--- Database/create_classes.sql
+++ Database/create_classes.sql
@@ -1,28 +1,5 @@
-CREATE TABLE time_entries (
-    entry_id INT NOT NULL UNIQUE,
-    title VARCHAR(100),
-    date_worked DATE,
-    time_begin TIME(),
-    time_ended TIME(),
-    project_id INT,
-    client_id INT,
-    task_id INT,
-    billable BOOLEAN,
-    PRIMARY KEY (entry_id)
-);
-
-CREATE TABLE project (
-    project_id INT NOT NULL UNIQUE,
-    title VARCHAR(100),
-    client_id INT,
-    hourly_rate FLOAT(24),
-    project_estimate FLOAT(24),
-    colour VARCHAR(20),
-    PRIMARY KEY (project_id)
-);
-
 CREATE TABLE client (
-    client_id INT NOT NULL UNIQUE,
+    client_id INT NOT NULL UNIQUE AUTO_INCREMENT,
     client_name VARCHAR(100),
     default_hourly_rate FLOAT(24),
     contact_name VARCHAR(100),
@@ -35,9 +12,36 @@ CREATE TABLE client (
     PRIMARY KEY (client_id)
 );
 
+CREATE TABLE project (
+    project_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    title VARCHAR(100),
+    client_id INT NOT NULL,
+    hourly_rate FLOAT(24),
+    project_estimate FLOAT(24),
+    colour VARCHAR(20),
+    PRIMARY KEY (project_id),
+    FOREIGN KEY (client_id) REFERENCES client(client_id)
+);
+
 CREATE TABLE task (
-    task_id INT NOT NULL UNIQUE,
+    task_id INT NOT NULL UNIQUE AUTO_INCREMENT,
     task_name VARCHAR(50),
     task_hourly_rate FLOAT(24),
     PRIMARY KEY (task_id)
+);
+
+CREATE TABLE time_entries (
+    entry_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    title VARCHAR(100),
+    date_worked DATE,
+    time_begin TIME,
+    time_ended TIME,
+    project_id INT NOT NULL,
+    client_id INT NOT NULL,
+    task_id INT NOT NULL,
+    billable BOOLEAN,
+    PRIMARY KEY (entry_id),
+    FOREIGN KEY (project_id) REFERENCES project(project_id),
+    FOREIGN KEY (client_id) REFERENCES client(client_id),
+    FOREIGN KEY (task_id) REFERENCES task(task_id)
 );
