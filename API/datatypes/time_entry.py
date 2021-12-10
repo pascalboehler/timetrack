cef7424861d056def23f24298351a340ffa7500c
Pascal Böhler <pascal.boehler@hotmail.de>
2021-12-10 17:57:58 +0100

    feat: Added get methods to savely access variables + made class vars private to prevent unwanted read write access

--- API/datatypes/time_entry.py
+++ API/datatypes/time_entry.py
@@ -1,13 +1,13 @@
 class TimeEntry:
-    title: str
-    date_begin: str
-    time_begin: str
-    date_end: str
-    time_end: str
-    project_id: int
-    client_id: int
-    task_id: int
-    billable: bool
+    _title: str
+    _date_begin: str
+    _time_begin: str
+    _date_end: str
+    _time_end: str
+    _project_id: int
+    _client_id: int
+    _task_id: int
+    _billable: bool
 
     def __init__(self, title, date_begin, time_begin, date_end, time_end, client_id, project_id = None, task_id = None, billable = False):
         # init optional parameters if set otherwise set to default
@@ -24,12 +24,36 @@ class TimeEntry:
         else:
             self.billable = True
         
-        self.title = title
-        self.date_begin = date_begin
-        self.time_begin = time_begin
-        self.date_end = date_end
-        self.time_end = time_end
-        self.client_id = client_id
+        self._title = title
+        self._date_begin = date_begin
+        self._time_begin = time_begin
+        self._date_end = date_end
+        self._time_end = time_end
+        self._client_id = client_id
     
     def __str__(self):
-        return f"{self.title}"
+        return f"{self._title}"
+
+    def getTitle(self):
+        return self._title
+
+    def getDateBegin(self):
+        return self._date_begin
+
+    def getTimeBegin(self):
+        return self._time_begin
+
+    def getDateEnd(self):
+        return self._date_end
+
+    def getTimeEnd(self):
+        return self._time_end
+
+    def getClientID(self):
+        return self._client_id
+
+    def getTaskID(self):
+        return self._task_id
+
+    def getProjectID(self):
+        return self._project_id
