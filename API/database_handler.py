eb935ad9f10901e2478b7cef3131d62981530af0
Pascal Böhler <pascal.boehler@hotmail.de>
2021-12-14 11:13:31 +0100

    feat: Added if statement to only run testing code when database_handler is called as main (for testing)

--- API/database_handler.py
+++ API/database_handler.py
@@ -361,46 +361,46 @@ class DatabaseHandler:
         except Error as err:
             print(f"Error: '{err}'")
 
+if __name__ == "__main__":
+    database = DatabaseHandler("./API/.env")
 
-database = DatabaseHandler("./API/.env")
+    print("--- TIME ENTRY ---")
+    new_time_entry = database.read_time_entry(1)
+    print(new_time_entry.getTitle())
 
-print("--- TIME ENTRY ---")
-new_time_entry = database.read_time_entry(1)
-print(new_time_entry.getTitle())
+    all_time_entries = database.read_all_time_entries()
 
-all_time_entries = database.read_all_time_entries()
+    for entry in all_time_entries:
+        print(entry.getTitle())
 
-for entry in all_time_entries:
-    print(entry.getTitle())
+    print("--- TASK ---")
 
-print("--- TASK ---")
+    new_task = database.read_task(1)
+    print(new_task.getTaskName())
 
-new_task = database.read_task(1)
-print(new_task.getTaskName())
+    all_tasks = database.read_all_tasks()
 
-all_tasks = database.read_all_tasks()
+    for task in all_tasks:
+        print(task.getTaskName())
 
-for task in all_tasks:
-    print(task.getTaskName())
+    print("--- CLIENT ---")
 
-print("--- CLIENT ---")
+    new_client = database.read_client(1)
+    print(new_client.getClientName())
 
-new_client = database.read_client(1)
-print(new_client.getClientName())
+    all_clients = database.read_all_clients()
 
-all_clients = database.read_all_clients()
+    for client in all_clients:
+        print(client.getClientName())
 
-for client in all_clients:
-    print(client.getClientName())
+    print("--- PROJECT ---")
 
-print("--- PROJECT ---")
+    new_project = database.read_project(1)
 
-new_project = database.read_project(1)
+    print(new_project.getTitle())
 
-print(new_project.getTitle())
+    all_projects = database.read_all_projects()
 
-all_projects = database.read_all_projects()
-
-for project in all_projects:
-    print(project.getTitle())
+    for project in all_projects:
+        print(project.getTitle())
 
