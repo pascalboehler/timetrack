22a46e808e5d07e169247601a5a1124ed22f47ee
Pascal Böhler <pascal.boehler@hotmail.de>
2021-12-17 10:46:42 +0100

    feat: Fixed date and file handling errors in logs.py

--- API/logs.py
+++ API/logs.py
@@ -1,32 +1,36 @@
-import datetime as date
+from datetime import date, datetime, time
+import os
+from os.path import exists as file_exists
 
 class Log:
-    # logging uses greenwhich main time!
-
-    timezone: date.tzinfo
-    class_logging: str
 
     def __init__(self, class_logging):
-        self.appendLine("Logging started")
-        self.timezone = date.tzinfo.utcoffset(+1) # Timezone BERLIN, AMSTERDAM
-        self.class_logging = class_logging
+        self._class_logging = class_logging
+        self.appendLine(f"{date.today()} - {self._class_logging} - INFO - Logging started\n")
 
     def debug(self, message):
-        self.appendLine(f"{date.today(self.timezone)} - {self.class_logging} - DEBUG - {message}")
+        self.appendLine(f"{date.today()} - {self._class_logging} - DEBUG - {message}\n")
 
     def info(self, message):
-        self.appendLine(f"{date.today(self.timezone)} - {self.class_logging} - INFO - {message}")
+        self.appendLine(f"{date.today()} - {self._class_logging} - INFO - {message}\n")
 
     def warning(self, message):
-        self.appendLine(f"{date.today(self.timezone)} - {self.class_logging} - WARNING - {message}")
+        self.appendLine(f"{date.today()} - {self._class_logging} - WARNING - {message}\n")
 
     def error(self, message):
-        self.appendLine(f"{date.today(self.timezone)} - {self.class_logging} - ERROR - {message}")
+        self.appendLine(f"{date.today()} - {self._class_logging} - ERROR - {message}\n")
 
     def critical(self, message):
-        self.appendLine(f"{date.today(self.timezone)} - {self.class_logging} - CRITICAL - {message}")
+        self.appendLine(f"{date.today()} - {self._class_logging} - CRITICAL - {message}\n")
 
     def appendLine(self,line):
-        file = open(f"./logs/{date.today(self.timezone).year}/{date.today(self.timezone).month}/{date.today(self.timezone)}.log")
-        file.write(line)
-        file.close() # IMPORTANT: Close file after writing, to prevent issues with different instances of this class!
+        if file_exists(f"./logs/{date.today().year}/{date.today().month}/{date.today()}.log"):
+            file = open(f"./logs/{date.today().year}/{date.today().month}/{date.today()}.log", 'a')
+            file.write(line)
+            file.close() # IMPORTANT: Close file after writing, to prevent issues with different instances of this class!
+        else:
+            if not file_exists(f"./logs/{date.today().year}/{date.today().month}"):
+                os.makedirs(f"./logs/{date.today().year}/{date.today().month}")
+            file = open(f"./logs/{date.today().year}/{date.today().month}/{date.today()}.log", 'w')
+            file.write(line)
+            file.close() # IMPORTANT: Close file after writing, to prevent issues with different instances of this class!
