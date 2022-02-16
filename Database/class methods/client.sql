c7751586b3a3aadec965f4ec76dcf4c37b7d61b0
Pascal Böhler <pascal.boehler@hotmail.de>
2022-02-16 11:07:59 +0100

    feat: Added method to update a clients information in the database

--- Database/class methods/client.sql
+++ Database/class methods/client.sql
@@ -16,3 +16,17 @@ VALUES (
 DELETE FROM client WHERE client_id = 2;
 
 SELECT * FROM client WHERE client_id = 1;
+
+UPDATE client
+SET
+    client_name = "", 
+    contact_name = "",
+    contact_phone = "",
+    default_hourly_rate = "",
+    billing_address_street_and_housenumber = "",
+    billing_address_postal_code = "",
+    billing_address_city = "",
+    billing_address_state = "",
+    billing_address_country = ""
+WHERE
+    client_id = 1
